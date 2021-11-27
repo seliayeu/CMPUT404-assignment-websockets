49f0a611e5edd05bbec6c4519286a9dc52964304
danila seliayeu <dan.seliayeu@gmail.com>
2021-11-26 22:45:38 -0700

    update requirements and finish assignment

--- sockets.py
+++ sockets.py
@@ -13,8 +13,12 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+# Modifications to the code licensed under CC BY-SA 4.0 by Danila Seliayeu, 
+# 2021 https://creativecommons.org/licenses/by-sa/4.0/
+
 import flask
-from flask import Flask, request
+from flask import Flask, request, redirect
+from flask.json import jsonify
 from flask_sockets import Sockets
 import gevent
 from gevent import queue
@@ -25,6 +29,25 @@ import os
 app = Flask(__name__)
 sockets = Sockets(app)
 app.debug = True
+
+# following code taken from https://github.com/abramhindle/WebSocketsExamples/blob/master/chat.py
+# which is written by Abram Hindle and licensed under the Apache License Version 2.0
+def send_all(msg):
+    for client in clients:
+        client.put( msg )
+
+def send_all_json(obj):
+    send_all( json.dumps(obj) )
+
+clients = list()
+class Client:
+    def __init__(self):
+        self.queue = queue.Queue();
+    def put(self, v):
+        self.queue.put_nowait(v)
+    def get(self):
+        return self.queue.get()
+
 
 class World:
     def __init__(self):
@@ -63,25 +86,48 @@ myWorld = World()
 
 def set_listener( entity, data ):
     ''' do something with the update ! '''
+    send_all_json({ entity: data })
 
 myWorld.add_set_listener( set_listener )
         
 @app.route('/')
 def hello():
-    '''Return something coherent here.. perhaps redirect to /static/index.html '''
-    return None
+    return redirect("/static/index.html") 
 
 def read_ws(ws,client):
     '''A greenlet function that reads from the websocket and updates the world'''
-    # XXX: TODO IMPLEMENT ME
-    return None
+    msg = ws.receive()
+    try:
+        while True:
+            msg = ws.receive()
+            print("WS RECV: %s" % msg)
+            if (msg is not None):
+                packet = json.loads(msg)
+                for entity in packet:
+                    myWorld.set(entity, packet[entity])
+                send_all_json(packet)
+            else:
+                break
+    except Exception as e:
+        print(e)
 
 @sockets.route('/subscribe')
 def subscribe_socket(ws):
-    '''Fufill the websocket URL of /subscribe, every update notify the
+    '''Fufill the websockjAet URL of /subscribe, every update notify the
        websocket and read updates from the websocket '''
-    # XXX: TODO IMPLEMENT ME
-    return None
+    print("uhh")
+    client = Client()
+    clients.append(client)
+    g = gevent.spawn(read_ws, ws, client)
+    try:
+        while True:
+            msg = client.get()
+            ws.send(msg)
+    except Exception as e:
+        print("WS Error %s" % e)
+    finally:
+        clients.remove(client)
+        gevent.kill(g)
 
 
 # I give this to you, this is how you get the raw body/data portion of a post in flask
@@ -99,23 +145,35 @@ def flask_post_json():
 @app.route("/entity/<entity>", methods=['POST','PUT'])
 def update(entity):
     '''update the entities via this interface'''
-    return None
+    print(myWorld.world())
+    if request.method == "PUT":
+        res = request.json
+        if not res: 
+            res = flask_post_json()
+        myWorld.set(entity, res)
+        return jsonify(myWorld.get(entity))
+    elif request.method == "POST":
+        res = request.json
+        if not res: res = flask_post_json()
+        for key in res:
+            myWorld = update(entity, key, res[key])
+        return jsonify(myWorld.world())
 
 @app.route("/world", methods=['POST','GET'])    
 def world():
-    '''you should probably return the world here'''
-    return None
+    print(myWorld.world())
+    print('arr')
+    return jsonify(myWorld.world())
 
 @app.route("/entity/<entity>")    
 def get_entity(entity):
-    '''This is the GET version of the entity interface, return a representation of the entity'''
-    return None
+    return jsonify(myWorld.get(entity))
 
 
 @app.route("/clear", methods=['POST','GET'])
 def clear():
-    '''Clear the world out!'''
-    return None
+    myWorld.clear()
+    return jsonify(myWorld.world())
 
 
 
